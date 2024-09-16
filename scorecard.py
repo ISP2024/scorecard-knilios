cf2c2875c1ec1fac3530844e25ea8bf856c7a6a3
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-09-16 15:31:12 +0700

    Finished working code

--- scorecard.py
+++ scorecard.py
@@ -18,21 +18,34 @@ Observe how the type hint helps it perform static checking.
 
 """
 
+from typing import Iterator
 
-class Scorecard:
+class Scorecard(Iterator):
     """Accumulate scores and compute their average."""
 
     def __init__(self):
         """Iniiialize a new Scorecard."""
-        self.scores = []
+        self.scores: list[(float | int)] = []
 
-    def add_score(self, score):
+    def add_score(self, score: float):
         """Add a score to the Scorecard."""
         self.scores.append(score)
 
-    def average(self):
+    def average(self) -> float:
         """Return the average of all scores, 0 if no scores."""
         return sum(self.scores)/max(1,len(self.scores))
+    
+    def __len__(self) -> int:
+        """Return the number of scores."""
+        return len(self.scores)
+    
+    def __next__(self) -> Iterator:
+        """Return an iterator of the scores."""
+        return iter(self.scores)
+    
+    def __iter__(self) -> Iterator:
+        """Return an iterator of the scores."""
+        return iter(self.scores)
 
 
 def print_scores(score_card):
@@ -46,12 +59,12 @@ def print_scores(score_card):
         print(score)
 
 
-def ordinal(num):
+def ordinal(num) -> str:
     """Return the ordinal value of an integer; works for numbers up to 20.
 
     For examples: ordinal(1) is '1st', ordinal(2) is '2nd'.
     """
-    suffixes = {1: "st", 2: "nd", 3: "rd"}
+    suffixes:dict[int, str] = {1: "st", 2: "nd", 3: "rd"}
     return str(num) + suffixes.get(num, "th")
 
 
@@ -62,8 +75,8 @@ if __name__ == "__main__":
     print("Input 3 scores.")
     for count in range(1,4):
         score = input(f"input {ordinal(count)} score: ")
-        scorecard.add_score(score)
+        scorecard.add_score(float(score))
 
-    print("The average is " + scorecard.average())
+    print("The average is " + str(scorecard.average()))
 
     print_scores(scorecard)
